bcb2b3442a75a588e73be83cf3279b00679cb064
Cehmenang <heaven.cehwin12@gmail.com>
2026-03-20 14:36:34 +0700

    mengubah semua fetch ke server side

--- src/user/service/user/user.service.ts
+++ src/user/service/user/user.service.ts
@@ -22,7 +22,7 @@ export class UserService {
 
     signToken({ username, id }: { username: string, id: string }, response: Response){
         const token = this.jwtService.sign({ id, username})
-        response.cookie('access_token', token, { httpOnly: true, secure: false, maxAge: 1000 * 3600 })
+        response.cookie('access_token', token, { httpOnly: true, secure: true, maxAge: 1000 * 3600, sameSite: 'none' })
         return token
     }
 
