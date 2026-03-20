98a28a68542d5ec2bbc765dc401febeef606398f
Cehmenang <heaven.cehwin12@gmail.com>
2026-03-20 14:06:51 +0700

    ganti enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser({ secret: process.env.COOKIE_SECRET! }))
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({ origin: 'https://cehwin-ai-learning.vercel.app', credentials: true });
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
