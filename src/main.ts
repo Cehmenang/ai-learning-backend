6e62d27671c5bf70377911cba195b77a86d1139f
Cehmenang <heaven.cehwin12@gmail.com>
2026-04-04 14:53:12 +0700

    Mengubah Port Backend

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser({ secret: process.env.COOKIE_SECRET! }))
   app.enableCors({ origin: 'https://cehwin.cloud', credentials: true });
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(4000);
 }
 bootstrap();
